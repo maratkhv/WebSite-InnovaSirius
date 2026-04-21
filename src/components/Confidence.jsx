2af062e9677da5141d1c9eaf7dc47e3fdc23ead8
Plufzepuf <6esed4@mail.ru>
2026-04-21 22:24:36 +0300

    Добавлено новое лого в Conf и текст в Footer

--- src/components/Confidence.jsx
+++ src/components/Confidence.jsx
@@ -3,9 +3,10 @@ import logo1 from '../assets/logos/ip_a_diapdi_page-0001_1_1.png';
 import logo2 from '../assets/logos/Logo_01_WB_page-0001.jpg';
 import logo3 from '../assets/logos/head_logo_fasie.png';
 import logo4 from '../assets/logos/elron.png';
+import logo5 from '../assets/logos/Group 1350.png';
 
 export default function Confidence() {
-  const logos = [logo1, logo2, logo3, logo4];
+  const logos = [logo1, logo2, logo3, logo4, logo5];
 
   return (
     <section className="conf">
